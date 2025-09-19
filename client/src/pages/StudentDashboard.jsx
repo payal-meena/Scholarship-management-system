a0fc02e7d8b4b5435c7f8977ebc2ac7efae72183
Yukta-Yadav02 <yuktay.bca2023@ssism.org>
2025-09-19 22:00:36 +0530

    add heading

--- client/src/pages/StudentDashboard.jsx
+++ client/src/pages/StudentDashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const StudentDashboard = () => {
   return (
     <div className='flex min-h-screen bg-gray-100'>
-        
+        <h1>Student Dashboard</h1>
 
     </div>
   )
