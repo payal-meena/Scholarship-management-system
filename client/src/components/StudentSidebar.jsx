c8e3bb241b381d987fa78fbd87a2c2ed539902ad
payal-meena <payalm.bca2023@ssism.org>
2025-09-21 18:46:55 +0530

    separate student sidebar

---
+++ client/src/components/StudentSidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const StudentSidebar = () => {
+  return (
+    <div className='w-64 bg-gray-800 text-white min-h-screen p-6 space-y-4'> 
+    <h2 className='text-lg font-bold'>Student Dashboard</h2>
+        <ul className='space-y-3'>
+            <li><Link to="/student/apply">Apply Scholarship</Link> </li>
+            <li><Link to="/student/my-applications">My Applications</Link></li>
+            <li><Link to="/student/profile">Profile</Link></li>
+            <li><Link to="/student/logout">Logout</Link></li>
+        </ul>
+    </div>
+  )
+}
+
+export default StudentSidebar
