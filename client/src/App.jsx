0476cdadd09daa3e37bd16aa97b192f922a45d99
payal-meena <payalm.bca2023@ssism.org>
2025-09-21 19:39:28 +0530

    Add Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import StudentLogin from './pages/StudentLogin'
 import StudentSignupPage from './pages/StudentSignupPage'
 import AdminLogin from './pages/AdminLogin'
 import Home from './pages/Home'
-import StudentDashboard from './pages/StudentDashboard'
+import StudentLayout from './Layout/StudentLayout'
+import AdminLayout from './Layout/AdminLayout'
 
 const App = () => {
   return (
@@ -16,12 +17,18 @@ const App = () => {
       <Route path="/admin-login" element={<AdminLogin />}/>
       <Route path='/login-page' element={<LandingPage />}/>
     
-      <Route path ="/student/*" element={<StudentDashboard />}>
+      <Route path ="/student/*" element={<StudentLayout />}>
         <Route path='apply' element={<h2>Apply Scholarship </h2>} />
         <Route path='my-applications' element={<h2>My Applications</h2>}/>
         <Route path='profile' element={<h2>Profile</h2>}/>
       </Route>
 
+      <Route path ="/admin/*" element={<AdminLayout />}>
+        <Route path='manage-scholarships' element={<h2>Manage Scholarships</h2>}/>
+        <Route path='manage-students' element={<h2>Manage Students</h2>} />
+        <Route path='view-applications' element={<h2>View Applications</h2>}/>
+        <Route path='profile' element={<h2> Profile</h2>}/>
+      </Route>
 
     </Routes>
   )
