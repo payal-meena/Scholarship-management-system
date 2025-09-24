441ed7b5d9b88be73df83fa3d18ac8a96a905b28
payal-meena <payalm.bca2023@ssism.org>
2025-09-24 19:01:05 +0530

    add style to sidebar

--- client/src/components/StudentSidebar.jsx
+++ client/src/components/StudentSidebar.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 const StudentSidebar = () => {
+  const menu = [
+    {path: "/student/apply",label: "Apply Scholarship"},
+    {path: "/student/my-applications", label: "My Applications"},
+    {path: "/student/profile" , label: "Profile"},
+    {path: "/student/logout" , label: "Logout"},
+  ];
+
   return (
-    <div className='w-64 bg-gray-800 text-white p-6 space-y-4'> 
-    <h2 className='text-lg font-bold'>Student Dashboard</h2>
-        <ul className='space-y-3'>
-            <li><Link to="/student/apply">Apply Scholarship</Link> </li>
-            <li><Link to="/student/my-applications">My Applications</Link></li>
-            <li><Link to="/student/profile">Profile</Link></li>
-            <li><Link to="/student/logout">Logout</Link></li>
-        </ul>
-    </div>
+    <aside className='w-64 bg-gray-800 text-white min-h-screen p-6 flex flex-col justify-between'> 
+        <div>
+          <h2 className='text-lg font-bold mb-6'>Student Menu</h2>
+          <nav className='space-y-3'>
+              {menu.map((item) => (
+                <NavLink key={item.path}
+                to={item.path}
+                className={({isActive}) => `block px-4 py-2 rounded hover:bg-blue-500 transition ${isActive ? "bg-blue-600 font-semibold" : "" }`}>
+                  {item.label}
+                </NavLink>
+              ))}
+          </nav>
+        </div>
+
+        <div>
+          <NavLink to="/" className="block px-4 py-2 rounded bg-red-600 hover:bg-red-700 transition text-center">
+            Logout
+          </NavLink>
+        </div>
+    </aside>
   )
 }
 
